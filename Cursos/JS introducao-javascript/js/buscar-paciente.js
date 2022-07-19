cf735c87d4a2ac29d1e5922a75b0c3e0b01f197c
VolutiRodguin <anzolinrodrigo@gmail.com>
2022-07-19 15:29:17 -0300

    Voluti - Update Diário

---
+++ Cursos/JS introducao-javascript/js/buscar-paciente.js
@@ -0,0 +1,26 @@
+var botaoAdicionarPaciente = document.querySelector("#buscar-paciente")
+botaoAdicionarPaciente.addEventListener("click", function () {
+    var xhr = new XMLHttpRequest()
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes")
+    xhr.addEventListener("load", function () {
+
+        if (xhr.status == 200) {
+            var erroAjax = document.querySelector("#erro-ajax")
+            erroAjax.classList.add("invisivel")
+            
+            var resposta = xhr.responseText
+            var pacientes = JSON.parse(resposta)
+            
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente)
+            })
+        }else {
+            console.log(xhr.status)
+            console.log(xhr.responseText)
+            erroAjax.classList.remove("invisivel")
+            
+        }
+    })
+    xhr.send()
+})
--- Cursos/JS introducao-javascript/js/form.js
+++ Cursos/JS introducao-javascript/js/form.js
@@ -5,24 +5,24 @@ botaoAdicionar.addEventListener("click", function (event) {
     var form = document.querySelector("#form-adiciona")
     var paciente = pegaPacienteForm(form)
 
-    var pacienteTr = montaTr(paciente)
-
     var erros = validaPaciente(paciente)
     
     if(erros.length > 0){
         exibeMensagensDeErro(erros)
         return
     }
-
-
-    var tabela = document.querySelector("#tabela-pacientes")
-    
-    tabela.appendChild(pacienteTr)
     
     // form.reset()
     var mensagensErro = document.querySelector("#mensagens-erro")
     mensagensErro.innerHTML = ""
 })
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente)
+    var tabela = document.querySelector("#tabela-pacientes")
+    tabela.appendChild(pacienteTr)
+    
+}
 
 function exibeMensagensDeErro(erros){
     var ul = document.querySelector("#mensagens-erro")
