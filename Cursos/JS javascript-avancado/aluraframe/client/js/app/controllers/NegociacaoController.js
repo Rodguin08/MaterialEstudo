e8f24716ce96029108d3852a42d3b2aab0cc3b37
VolutiRodguin <anzolinrodrigo@gmail.com>
2022-08-03 16:21:48 -0300

    Voluti - Update diario

--- Cursos/JS javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Cursos/JS javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,86 +1,106 @@
 class NegociacaoController {
-
+    
     constructor() {
-
+        
         let $ = document.querySelector.bind(document);
-
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
+         
         this._listaNegociacoes = new Bind(
-            new ListaNegociacoes(),
-            new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia', 'ordena', 'inverteOrdem');
-
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia' , 'ordena', 'inverteOrdem');
+       
         this._mensagem = new Bind(
             new Mensagem(), new MensagemView($('#mensagemView')),
-            'texto');
+            'texto');    
+            
+        this._ordemAtual = '';
 
-        this._ordemAtual = ''
+        this._service = new NegociacaoService();
 
-        ConnectionFactory
-            .getConnection()
-            .then(connection => new NegociacaoDao(connection))
-            .then(dao => dao.listaTodos())
-            .then(negociacoes => negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao)))
+        this._init();
+
+    }
+    
+    _init() {
+
+        this._service
+            .lista()
+            .then(negociacoes => 
+                negociacoes.forEach(negociacao => 
+                    this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => this._mensagem.texto = erro);
+
+       setInterval(() => {
+           this.importaNegociacoes();
+       }, 3000);                
+        
     }
 
     adiciona(event) {
+        
         event.preventDefault();
+        
+        let negociacao = this._criaNegociacao();
 
-        ConnectionFactory.getConnection().then(connection => {
-            let negociacao = this._criaNegociacao()
-            new NegociacaoDao(connection).adiciona(negociacao).then(() => {
-                this._listaNegociacoes.adiciona(negociacao)
-                this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._service
+            .cadastra(negociacao)
+            .then(mensagem => {
+                this._listaNegociacoes.adiciona(negociacao);
+                this._mensagem.texto = mensagem;
                 this._limpaFormulario();
             })
-        }).catch(erro => this._mensagem.texto = erro)
-    }
-
-    importaNegociacoes() {
-
-
-        let service = new NegociacaoService();
-        service
-            .obterNegociacoes()
-            .then(negociacoes => negociacoes.forEach(negociacao => {
-                this._listaNegociacoes.adiciona(negociacao);
-                this._mensagem.texto = 'Negociações do período importadas'
-            }))
             .catch(erro => this._mensagem.texto = erro);
     }
+    
+    importaNegociacoes() {
 
-    apaga() {
-
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._service
+            .importa(this._listaNegociacoes.negociacoes)
+            .then(negociacoes => negociacoes.forEach(negociacao => {
+                this._listaNegociacoes.adiciona(negociacao);
+                this._mensagem.texto = 'Negociações do período importadas'   
+            }))
+            .catch(erro => this._mensagem.texto = erro);                              
     }
-
+    
+    apaga() {
+        
+        this._service
+            .apaga()
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();                
+            })
+            .catch(erro => this._mensage.texto = erro);
+    }
+    
     _criaNegociacao() {
-
+        
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value));
+            parseFloat(this._inputValor.value));    
     }
-
+    
     _limpaFormulario() {
-
+     
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-        this._inputData.focus();
+        this._inputData.focus();   
     }
-
+    
     ordena(coluna) {
-
-        if (this._ordemAtual == coluna) {
-            this._listaNegociacoes.inverteOrdem();
+        
+        if(this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem(); 
         } else {
-            this._listaNegociacoes.ordena((p, s) => p[coluna] - s[coluna]);
+            this._listaNegociacoes.ordena((p, s) => p[coluna] - s[coluna]);    
         }
-        this._ordemAtual = coluna;
+        this._ordemAtual = coluna;    
     }
 }
